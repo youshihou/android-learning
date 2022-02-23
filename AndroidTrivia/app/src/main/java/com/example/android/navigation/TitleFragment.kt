5d4f1dffb4778bb9ff544d918048da6c2bd05c71
Ankui <youshihou5@gmail.com>
2022-02-23 22:20:25 +0800

    update code

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -68,7 +68,8 @@ class TitleFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
             inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener {
-            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            it.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
         setHasOptionsMenu(true)
         return binding.root
